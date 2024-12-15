33eed20a9c014020809b7caca4cee5d80c748293
24517209 <161823886+24517209@users.noreply.github.com>
2024-12-15 14:38:34 +0800

    Update Visitor.java

--- OOP-A2/src/Visitor.java
+++ OOP-A2/src/Visitor.java
@@ -1,15 +1,19 @@
+import java.util.Objects;
+
 public class Visitor extends Person {
     private String ticketId;
     private String visitTime;
+    private String preferredThrillLevel;
 
     public Visitor() {
         super();
     }
 
-    public Visitor(String name, int age, String gender, String ticketId, String visitTime) {
+    public Visitor(String name, int age, String gender, String ticketId, String visitTime, String preferredThrillLevel) {
         super(name, age, gender);
         this.ticketId = ticketId;
         this.visitTime = visitTime;
+        this.preferredThrillLevel = preferredThrillLevel;
     }
 
     public String getTicketId() {
@@ -28,12 +32,62 @@ public class Visitor extends Person {
         this.visitTime = visitTime;
     }
 
-    public String toCsv() {
-        return getName() + "," + getAge() + "," + getGender() + "," + ticketId + "," + visitTime;
+    public String getPreferredThrillLevel() {
+        return preferredThrillLevel;
     }
+
+    public void setPreferredThrillLevel(String preferredThrillLevel) {
+        this.preferredThrillLevel = preferredThrillLevel;
+    }
+
+    
+    public String toCsv() {
+        return getName() + "," + getAge() + "," + getGender() + "," + ticketId + "," + visitTime + "," + preferredThrillLevel;
+    }
+
 
     public static Visitor fromCsv(String csv) {
         String[] parts = csv.split(",");
-        return new Visitor(parts[0], Integer.parseInt(parts[1]), parts[2], parts[3], parts[4]);
+        return new Visitor(parts[0], Integer.parseInt(parts[1]), parts[2], parts[3], parts[4], parts[5]);
+    }
+
+
+    public String toJson() {
+        return String.format("{\"name\": \"%s\", \"age\": %d, \"gender\": \"%s\", \"ticketId\": \"%s\", \"visitTime\": \"%s\", \"preferredThrillLevel\": \"%s\"}",
+                getName(), getAge(), getGender(), ticketId, visitTime, preferredThrillLevel);
+    }
+
+ 
+    public static Visitor fromJson(String json) {
+        try {
+            String[] parts = json.replace("{", "").replace("}", "").replace("\"", "").split(", ");
+            String name = parts[0].split(": ")[1];
+            int age = Integer.parseInt(parts[1].split(": ")[1]);
+            String gender = parts[2].split(": ")[1];
+            String ticketId = parts[3].split(": ")[1];
+            String visitTime = parts[4].split(": ")[1];
+            String preferredThrillLevel = parts[5].split(": ")[1];
+            return new Visitor(name, age, gender, ticketId, visitTime, preferredThrillLevel);
+        } catch (Exception e) {
+            throw new IllegalArgumentException("Invalid JSON format: " + json);
+        }
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Visitor visitor = (Visitor) obj;
+        return Objects.equals(getName(), visitor.getName()) && Objects.equals(ticketId, visitor.getTicketId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getName(), ticketId);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", Ticket ID: " + ticketId + ", Visit Time: " + visitTime + ", Preferred Thrill Level: " + preferredThrillLevel;
     }
 }
