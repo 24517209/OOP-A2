f592cec614a06d874282d7e4c58e59d6230fb918
24517209 <161823886+24517209@users.noreply.github.com>
2024-12-15 14:38:47 +0800

    Update VisitorComparator.java

--- OOP-A2/src/VisitorComparator.java
+++ OOP-A2/src/VisitorComparator.java
@@ -3,9 +3,12 @@ import java.util.Comparator;
 public class VisitorComparator implements Comparator<Visitor> {
     @Override
     public int compare(Visitor v1, Visitor v2) {
-        if (v1.getAge() != v2.getAge()) {
-            return Integer.compare(v1.getAge(), v2.getAge());
-        }
-        return v1.getName().compareTo(v2.getName());
+        int ageComparison = Integer.compare(v1.getAge(), v2.getAge());
+        if (ageComparison != 0) return ageComparison;
+
+        int nameComparison = v1.getName().compareTo(v2.getName());
+        if (nameComparison != 0) return nameComparison;
+
+        return v1.getTicketId().compareTo(v2.getTicketId());
     }
 }
